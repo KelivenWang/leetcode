5dc4f6d7b9c8007699bbaba6e4e8a3d520f568c1
KelivenWang <yoyorockman211043@gmail.com>
2022-09-03 02:14:28 +0800

    modified 350. for another solution

--- 350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
+++ 350. Intersection of Two Arrays II/350. Intersection of Two Arrays II.cpp
@@ -74,3 +74,26 @@ int main(){
 
     return 0;
 }
+
+/*
+another best solution 
+reference https://leetcode.com/problems/intersection-of-two-arrays-ii/discuss/82269/Short-Python-C%2B%2B
+
+vector<int> intersect(vector<int>& a, vector<int>& b) {
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
+    return a;
+}
+
+vector<int> intersect(vector<int>& a, vector<int>& b) {
+    unordered_map<int, int> ctr;
+    for (int i : a)
+        ctr[i]++;
+    vector<int> out;
+    for (int i : b)
+        if (ctr[i]-- > 0)
+            out.push_back(i);
+    return out;
+}
+ */
