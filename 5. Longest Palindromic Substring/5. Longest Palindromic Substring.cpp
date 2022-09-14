076ec6721194bab34686302b03d88cb72aa39d88
KelivenWang <yoyorockman211043@gmail.com>
2022-09-15 01:44:49 +0800

    2022.9.15 restart do leetcode

--- 5. Longest Palindromic Substring/5. Longest Palindromic Substring.cpp
+++ 5. Longest Palindromic Substring/5. Longest Palindromic Substring.cpp
@@ -8,28 +8,46 @@ class Solution {
 public:
     string longestPalindrome(string s) {
 
-        string substring = "";
-        if(s.length()<=1){
-            substring = s;
-            return substring;
+        if(s==""){
+            return "";
         }
 
-        for(int left=0; left<s.length(); left++){
-            int right = left;
-            vector<int> mpforsubstring(27);
-
-            while(right<s.length() && !mpforsubstring[s[right]-97]){
-
-                mpforsubstring[s[right]-97] = 1;
-                substring += s[right];
-                right++;
+        vector<vector<int> > dp(s.size(),vector<int>(s.size(),0));
+        int head = 0;
+        int trail = 0;
+        int max = 0;
+        // 初始化 dp 
+        for(int i=0; i<s.size(); i++){
+            dp[i][i] = 1; // 字串長度 = 1
+            if((i+1)<s.size() && s[i]==s[i+1]){
+                // 長度為 2 的子字串
+                dp[i][i+1] = 1;
             }
-
-            substring += s[right];
-            return substring;
         }
 
-        return substring;
+        for(int i=2; i<s.size(); i++){
+            // i 是第幾個從左上到右下的對角線 i = 0 表示主對角線
+            for(int j=0; j<s.size()-i; j++){
+                // j 是第幾行
+                if(dp[j+1][j+i-1]==1 && s[j]==s[j+i]){
+                    dp[j][j+i] = 1;
+                }
+            }
+        }
+
+        for(int i=0; i<s.size(); i++){
+            for(int j=i; j<s.size(); j++){
+                if(dp[i][j]){
+                    if(j-i>max){
+                        head = i;
+                        trail = j;
+                        max = j-i;
+                    }
+                }
+            }
+        }
+        return s.substr(head,trail+1-head);
+        
     }
 };
 
@@ -44,7 +62,7 @@ Explanation: "aba" is also a valid answer.
         如果左不等於右 -> 目前字母加入到 substring 中
         如果最左邊字母等於最右邊字母 -> return 答案
  */
-    string s = "babad";
+    string s = "abdca";
     Solution solution;
 
     cout<<solution.longestPalindrome(s)<<endl;
