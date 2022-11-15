3b73ae91c13b732702499942152399523e0c9881
KelivenWang <yoyorockman211043@gmail.com>
2022-11-15 12:35:45 +0800

    upload form home

---
+++ 36. Valid Sudoku/36. Valid Sudoku.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+    }
+private:
+    bool isRowRule(vector<vector<char>> &board, int n1, int n2){
+
+    }
+
+    bool isColumnRule(vector<vector<char>> &board, int n1, int n2){
+
+    }
+
+    bool isSquare(vector<vector<char>> &board, int n1, int n2){
+        
+    }
+};
+
+int main(){
+    return 0;
+}
