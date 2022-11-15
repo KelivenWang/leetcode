213d38a8d80459dbbfdbf943262dab2098025cd1
KelivenWang <yoyorockman211043@gmail.com>
2022-11-15 13:30:46 +0800

    upload from home

--- 36. Valid Sudoku/36. Valid Sudoku.cpp
+++ 36. Valid Sudoku/36. Valid Sudoku.cpp
@@ -5,19 +5,45 @@ using namespace std;
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+
+        int RowLength = board[0].size();
+        int ColumanLength = board.size();
+
+        if(isRowRule(board,RowLength,ColumanLength) && isColumnRule(board,RowLength,ColumanLength) && isSquare(board,RowLength,ColumanLength)){
+            return true;
+        }return false;
         
     }
 private:
-    bool isRowRule(vector<vector<char>> &board, int n1, int n2){
+    bool isRowRule(vector<vector<char>> &board, int &RowLength, int &ColumnLength){
+        vector<int> mapNumber(10,0);
+
+        for(int i=0; i<ColumnLength; i++){
+            for(int j=0; j<RowLength; j++){
+                int node = board[i][j];
+
+                if(node!= 46){
+                    // ascii '.' => 46
+                    // ascii '0' ~ '9' => 48~57
+                    // check wheather have number or '.'
+                    node = (node % 10) + 2; 
+                    mapNumber[node]++;
+                    if(mapNumber[node]>1){
+                        return false;
+                    }
+
+                }
+            }
+            
+        }return true;
+    }
+
+    bool isColumnRule(vector<vector<char>> &board, int &RowLength, int &ColumnLength){
 
     }
 
-    bool isColumnRule(vector<vector<char>> &board, int n1, int n2){
+    bool isSquare(vector<vector<char>> &board, int &RowLength, int &ColumnLength){
 
-    }
-
-    bool isSquare(vector<vector<char>> &board, int n1, int n2){
-        
     }
 };
 
